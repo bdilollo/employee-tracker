b642a53867fa79daf9f322e96c1fa8ecf8a8fd49
bdilollo <113383715+bdilollo@users.noreply.github.com>
2022-10-31 00:15:22 -0500

    overall JS structure complete--needs sql to fill in actual data

---
+++ utils/actions.js
@@ -0,0 +1,37 @@
+function viewDepartments() {
+    console.log('the view departments case works!')
+};
+
+function viewRoles() {
+    console.log('the view roles case works!')
+};
+
+function viewEmployees() {
+    console.log('the view employees case works!')
+};
+
+function addDepartment() {
+    console.log('the add department case works!')
+};
+
+function addRole() {
+    console.log('the add role case works!')
+};
+
+function addEmployee() {
+    console.log('the add employee case works!')
+};
+
+function updateEmployee() {
+    console.log('the update employee case works!')
+}
+
+module.exports = {
+    viewDepartments,
+    viewRoles,
+    viewEmployees,
+    addDepartment,
+    addRole,
+    addEmployee,
+    updateEmployee
+};
