65714133297dcc5fa8c2dfcbf70eb2b5d29e5777
bdilollo <113383715+bdilollo@users.noreply.github.com>
2022-11-01 19:19:07 -0500

    small fix - plural table names

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,14 +1,14 @@
-INSERT INTO department (name)
+INSERT INTO departments (name)
 VALUES  ('Sales'),
         ('Finance');
 
-INSERT INTO role (title, salary, department_id)
+INSERT INTO roles (title, salary, department_id)
 VALUES  ('Head of Sales', 100000, 1),
         ('Junior Salesperson', 65000, 1),
         ('Head of Finance', 100000, 2),
         ('Financial Analyst', 80000, 2);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES  ('John', 'Smith', 1, null),
         ('Candace', 'Winterbottom', 3, null),
         ('Sally', 'Jones', 2, 1),
